b91505bb0cde495cad05962b1626c492a6d96eff
Daniel Alfonso <danielalfonso@iMac-de-Daniel.local>
2025-12-05 21:59:55 -0500

    Cambio next.config.ts a next.config.mjs para Next 14

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 🔹 Copia aquí TODO lo que tienes dentro del objeto en next.config.ts
+  // por ejemplo:
+  // experimental: {},
+  // output: "standalone",
+};
+
+export default nextConfig;
